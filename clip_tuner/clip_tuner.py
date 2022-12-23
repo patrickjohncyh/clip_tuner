49769276e0ae190abd6828d480ddcbeabb5e84c9
patrickjohncyh <patrickjohncyh@icloud.com>
2022-12-23 22:41:38 +0800

    force to .cuda()

--- clip_tuner/clip_tuner.py
+++ clip_tuner/clip_tuner.py
@@ -57,7 +57,7 @@ class CLIPTuner:
         self.model, self.preprocess = clip.load("ViT-B/32", jit=False)  # Must set jit=False for training
         # , device=self.device,
         self.model.forward = distributed_forward.__get__(self.model, CLIP)
-        self.model = nn.parallel.DataParallel(self.model) if multi_gpu else self.model
+        self.model = nn.parallel.DataParallel(self.model.cuda()) if multi_gpu else self.model
         if comet_tracking:
             self.experiment = Experiment(comet_tracking)
         else:
